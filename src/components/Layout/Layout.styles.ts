e59eb9b72c3fa82a94eb2143439157b14c725c54
Karol <karol.perec@tsh.io>
2022-04-16 01:46:19 +0200

    chore: update packages

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,23 +1,27 @@
-import { styled } from '@mui/material';
+import { styled } from "@mui/material";
 
-export const Main = styled('main')(({ theme }) => ({
-  minHeight: 'calc(100% - 68px)',
-  backgroundColor: theme.palette.background.default,
+export const Main = styled("main")(({ theme }) => ({
+  minHeight: "calc(100% - 56px)",
   paddingTop: theme.spacing(2),
+  background: [
+    "radial-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url(/archive-g9a9eea1fd_1920.jpg) no-repeat center",
+  ],
+  [theme.breakpoints.up("sm")]: {
+    minHeight: "calc(100% - 64px)",
+  },
+  display: 'flex',
+  flexDirection: 'column'
 }));
 
-export const Offset = styled('div')(({ theme }) => ({
+export const Offset = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
   backgroundColor: theme.palette.background.default,
 }));
 
-export const MainContentContainer = styled('div')(({ theme }) => ({
-  maxWidth: 700,
-  marginLeft: 'auto',
-  marginRight: 'auto',
-
-  [theme.breakpoints.up('sm')]: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-  },
+export const MainContentContainer = styled("div")(({ theme }) => ({
+  width: '100%',
+  maxWidth: 800,
+  marginLeft: "auto",
+  marginRight: "auto",
+  backgroundColor: theme.palette.common.white,
 }));
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
+import { GlobalStyles } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <GlobalStyles
+        styles={{
+          "html, body, #__next": {
+            height: "100%",
+            padding: 0,
+            margin: 0,
+            fontFamily:
+              "Roboto, -apple-system, BlinkMacSystemFont, Segoe UI,  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+          },
+          "*": { boxSizing: "border-box" },
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
